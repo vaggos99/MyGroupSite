2ad7053ba51653516f35be7580a3c27271adba10
Vaggelis Koronis <korwnis718@gmail.com>
2021-01-05 16:28:08 +0200

    change header and root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :aposts
   resources :allposts
-   root to: "home#index"
+   root to: "aposts#index"
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
